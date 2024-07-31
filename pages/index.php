987af10bf82b6ddd0319c743fea803ff90848203
최도은 <choidoeun0102@gmail.com>
2024-08-01 00:44:44 +0900

    모든 페이지 php로 변경

--- pages/index.php
+++ pages/index.php
@@ -11,19 +11,19 @@
 <body>
     <!-- 헤더 시작 -->
     <header>
-        <a href="./index.html"><img class="logo" src="./logo.png" alt="logo" title="logo"></a>
+        <a href="./"><img class="logo" src="./logo.png" alt="logo" title="logo"></a>
         <ul>
             <li><a href="#">소개</a>
                 <ul>
-                    <li><a href="./sub01.html">달빛기행축제란</a></li>
+                    <li><a href="./sub01">달빛기행축제란</a></li>
                     <li><a href="#">지역별 달빛기행축제 안내</a></li>
-                    <li><a href="./sub03.html">코스소개</a></li>
+                    <li><a href="./sub03">코스소개</a></li>
                 </ul>
             </li>
             <li><a href="#">참여/소식</a>
                 <ul>
                     <li><a href="#">공지사항</a></li>
-                    <li><a href="./sub02.html">문화달력</a></li>
+                    <li><a href="./sub02">문화달력</a></li>
                     <li><a href="#">퀴즈 이벤트 참가</a></li>
                 </ul>
             </li>
