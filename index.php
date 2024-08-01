ec77cc6b6a124cda2775c841dda38664a387f50f
최도은 <choidoeun0102@gmail.com>
2024-08-01 20:37:11 +0900

    api 라우팅 처리

--- index.php
+++ index.php
@@ -6,29 +6,41 @@ $path[1] = isset($path[1]) ? $path[1] : null;
 $resource = explode("/", $path[0]);
 
 $page = "";
-switch ($resource[1]) {
-    case "":
-        $page = "./pages/index.php";
-        break;
-    case "sub01":
-        $page = "./pages/sub01.php";
-        break;
-    case "sub02":
-        $page = "./pages/sub02.php";
-        break;
-    case "sub03":
-        $page = "./pages/sub03.php";
-        break;
-    case "register":
-        $page = "./pages/register.php";
-        break;
-    case "login":
-        $page = "./pages/login.php";
-        break;
-    default:
-        echo "잘못된 접근입니다.";
-        break;
+if ($resource[1] == "api") {
+    switch ($resource[2]) {
+        case "register":
+            $page = "./api/register.php";
+            break;
+        default:
+            echo "잘못된 접근입니다.";
+            break;
+    }
+    include($page);
+} else {
+    switch ($resource[1]) {
+        case "":
+            $page = "./pages/index.php";
+            break;
+        case "sub01":
+            $page = "./pages/sub01.php";
+            break;
+        case "sub02":
+            $page = "./pages/sub02.php";
+            break;
+        case "sub03":
+            $page = "./pages/sub03.php";
+            break;
+        case "register":
+            $page = "./pages/register.php";
+            break;
+        case "login":
+            $page = "./pages/login.php";
+            break;
+        default:
+            echo "잘못된 접근입니다.";
+            break;
+    }
+    include("./componets/header.php");
+    include($page);
+    include("./componets/footer.php");
 }
-include ("./componets/header.php");
-include ($page);
-include ("./componets/footer.php");
