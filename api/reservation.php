4c419e38aa4aef557a004c9e80e7a323afbf23fe
최도은 <choidoeun0102@gmail.com>
2024-08-04 02:12:36 +0900

    코스 예약 시 course 테이블 인원 update 쿼리 추가

--- api/reservation.php
+++ api/reservation.php
@@ -9,6 +9,12 @@ $sql = "INSERT INTO reservation (course_idx, name, phone_number, email, personne
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$course_idx, $name, $phoneNumber, $email, $personnel_count]);
 
+$sql2 = "UPDATE course SET current_personnel = current_personnel + :personnel_count WHERE course_idx = :course_idx";
+$stmt2 = $pdo->prepare($sql2);
+$stmt2->bindParam(":personnel_count", $personnel_count);
+$stmt2->bindParam(":course_idx", $course_idx);
+$stmt2->execute();
+
 echo "예약이 완료되었습니다.";
 
 ?>
